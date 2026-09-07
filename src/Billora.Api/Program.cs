9ee6241fdc42f65bde1c0210edda8f752e77c43d
Toms <toms.vanders@gmail.com>
2026-09-07 13:43:37 +0200

    health check setup

--- src/Billora.Api/Program.cs
+++ src/Billora.Api/Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var conStr = builder.Configuration.GetConnectionString("Database");
+if (string.IsNullOrEmpty(conStr))
+{
+    throw new InvalidOperationException("Could not find connection string 'Database'.");
+}
+
+builder.Services.AddHealthChecks()
+    .AddSqlServer(
+    connectionString: conStr,
+    name: "sqlserver",
+    tags: ["ready"]);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapHealthChecks("health", new() { Predicate = _ => false});
+app.MapHealthChecks("health/ready", new() { Predicate = check => check.Tags.Contains("ready") });
 
 await app.RunAsync();
